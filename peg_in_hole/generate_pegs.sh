ee73a729e1978543634ec3d3bda1af6bc7d7e7e4
macmacal <mac.aleksandrowicz@gmail.com>
2024-03-12 18:02:59 +0100

    cleanup of generate_pegs.sh

--- peg_in_hole/generate_pegs.sh
+++ peg_in_hole/generate_pegs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 SCRIPT_PATH=./src/peg.scad
-OUTPUT_DIR=./meshes
+OUTPUT_DIR=./meshes/pegs
+
+VERTICES_LIST=(4 6 8 12)
 DIAMETER=40
 HEIGHT=100
 
-openscad -D vertices=4 -D diameter=$DIAMETER -D height=$HEIGHT $SCRIPT_PATH -o ${OUTPUT_DIR}/square_peg.stl
-openscad -D vertices=6 -D diameter=$DIAMETER -D height=$HEIGHT $SCRIPT_PATH -o ${OUTPUT_DIR}/hexagon_peg.stl
-openscad -D vertices=8 -D diameter=$DIAMETER -D height=$HEIGHT $SCRIPT_PATH -o ${OUTPUT_DIR}/octagon_peg.stl
-openscad -D vertices=12 -D diameter=$DIAMETER -D height=$HEIGHT $SCRIPT_PATH -o ${OUTPUT_DIR}/dodecagon_peg.stl
+
+for vertices in "${VERTICES_LIST[@]}"; do
+    echo ">>> UNIFORM ${vertices}-GONAL PRISM"
+    openscad -D vertices=$vertices -D diameter=$DIAMETER -D height=$HEIGHT $SCRIPT_PATH -o ${OUTPUT_DIR}/${vertices}gon_peg.stl
+done
