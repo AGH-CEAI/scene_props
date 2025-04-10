5973aea56677b9605665838b84ed25e69905fb41
macmacal <mac.aleksandrowicz@gmail.com>
2025-04-10 18:11:17 +0200

    script for generating tht compoments

---
+++ peg_in_hole/generate_tht_components.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+SCRIPT_PATH=./src/tht_component.scad
+OUTPUT_DIR=./meshes/tht_components
+mkdir -p $OUTPUT_DIR
+
+# Single block with pin
+BLOCK_SIDE=10
+BLOCK_HEIGHT=10
+PIN_DIAMETER=$((BLOCK_SIDE / 2))
+PIN_HEIGHT=$((BLOCK_HEIGHT * 2))
+DEFAULT_PIN=true
+
+# ------------------------------------------- #
+# ------------ THT 2x2, all pins ------------ #
+# ------------------------------------------- #
+COLS=2
+ROWS=2
+INVERTED_PINS="[]"
+NAME_SUFFIX=""
+
+echo ">>> DUMMY THT-COMPONENT ${COLS}x${ROWS}"
+openscad \
+        -D block_side=$BLOCK_SIDE \
+        -D block_height=$BLOCK_HEIGHT \
+        -D pin_diameter=$PIN_DIAMETER \
+        -D pin_height=$PIN_HEIGHT \
+        -D cols=$COLS \
+        -D rows=$ROWS \
+        -D default_pin=$DEFAULT_PIN \
+        -D inverted_pins="$INVERTED_PINS" \
+        $SCRIPT_PATH \
+        -o ${OUTPUT_DIR}/tht_${COLS}x${ROWS}${NAME_SUFFIX}.stl
+
+# ------------------------------------------- #
+# ------------ THT 2x3, all pins ------------ #
+# ------------------------------------------- #
+COLS=2
+ROWS=3
+INVERTED_PINS="[]"
+NAME_SUFFIX=""
+
+echo ">>> DUMMY THT-COMPONENT ${COLS}x${ROWS}"
+openscad \
+        -D block_side=$BLOCNAME_SUFFIX=""K_SIDE \
+        -D block_height=$BLOCK_HEIGHT \
+        -D pin_diameter=$PIN_DIAMETER \
+        -D pin_height=$PIN_HEIGHT \
+        -D cols=$COLS \
+        -D rows=$ROWS \
+        -D default_pin=$DEFAULT_PIN \
+        -D inverted_pins="$INVERTED_PINS" \
+        $SCRIPT_PATH \
+        -o ${OUTPUT_DIR}/tht_${COLS}x${ROWS}${NAME_SUFFIX}.stl
+
+# ------------------------------------------- #
+# ---------- THT 2x3, missing pins ---------- #
+# ------------------------------------------- #
+COLS=2
+ROWS=3
+INVERTED_PINS="[[0,0],[1,1]]"
+NAME_SUFFIX="_missing_pins"
+
+echo ">>> DUMMY THT-COMPONENT ${COLS}x${ROWS}"
+openscad \
+        -D block_side=$BLOCK_SIDE \
+        -D block_height=$BLOCK_HEIGHT \
+        -D pin_diameter=$PIN_DIAMETER \
+        -D pin_height=$PIN_HEIGHT \
+        -D cols=$COLS \
+        -D rows=$ROWS \
+        -D default_pin=$DEFAULT_PIN \
+        -D inverted_pins="$INVERTED_PINS" \
+        $SCRIPT_PATH \
+        -o ${OUTPUT_DIR}/tht_${COLS}x${ROWS}${NAME_SUFFIX}.stl
